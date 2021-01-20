adca1704e26ee4406cb7264ddd8ac7828e81f3cb
Diana Buffone <diana.pamelaet@gmail.com>
2021-01-20 11:24:13 -0500

    Complete iteration 1

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -19,7 +19,24 @@ class VendorTest < Minitest::Test
 
   def test_check_stock_with_item
     vendor = Vendor.new("Rocky Mountain Fresh")
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
 
     assert_equal 0, vendor.check_stock(item1)
+  end
+
+  def test_stock_items
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
+
+    vendor.stock(item1, 30)
+
+    assert_equal ({item1 => 30}), vendor.inventory
+    assert_equal 30, vendor.check_stock(item1)
+    assert_equal 0, vendor.check_stock(item2)
+
+    vendor.stock(item2, 12)
+
+    assert_equal ({item1 => 30, item2 => 12}), vendor.inventory
   end
 end
