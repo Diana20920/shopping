bd11eae97d9912d64369f1dea76f1eaab73a99db
Diana Buffone <diana.pamelaet@gmail.com>
2021-01-20 11:48:59 -0500

    Update test files

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -39,4 +39,39 @@ class VendorTest < Minitest::Test
 
     assert_equal ({item1 => 30, item2 => 12}), vendor.inventory
   end
+
+  def test_check_if_sell_item_by_name
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
+
+    vendor.stock(item1, 30)
+    vendor.stock(item2, 12)
+
+    assert vendor.sell_item?('Peach')
+    refute vendor.sell_item?('Raibows')
+  end
+
+  def test_return_potential_revenue
+    vendor1 = Vendor.new("Rocky Mountain Fresh")
+
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: "$0.50"})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+
+    vendor1.stock(item1, 35)
+    vendor1.stock(item2, 7)
+
+    vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    vendor2.stock(item4, 50)
+    vendor2.stock(item3, 25)
+
+    vendor3 = Vendor.new("Palisade Peach Shack")
+    vendor3.stock(item1, 65)
+
+    assert_equal 29.75, vendor1.potential_revenue
+    assert_equal 345.00, vendor2.potential_revenue
+    assert_equal 48.75, vendor3.potential_revenue
+  end
 end
