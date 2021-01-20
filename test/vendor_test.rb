a6dc7f6e516e550d71bc031283402d41f29c2ac6
Diana Buffone <diana.pamelaet@gmail.com>
2021-01-20 11:10:22 -0500

    Test until item needed

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -9,4 +9,17 @@ class VendorTest < Minitest::Test
 
     assert_instance_of Vendor, vendor
   end
+
+  def test_it_has_attributes
+    vendor = Vendor.new("Rocky Mountain Fresh")
+
+    assert_equal "Rocky Mountain Fresh", vendor.name
+    assert_equal ({}), vendor.inventory
+  end
+
+  def test_check_stock_with_item
+    vendor = Vendor.new("Rocky Mountain Fresh")
+
+    assert_equal 0, vendor.check_stock(item1)
+  end
 end
