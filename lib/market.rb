293c393efeb89f324ed6d2af8c6c8c5f6cd4434b
Diana Buffone <diana.pamelaet@gmail.com>
2021-01-20 12:17:52 -0500

    Complete iteration 3

--- lib/market.rb
+++ lib/market.rb
@@ -22,4 +22,38 @@ class Market
       vendor.sell_item?(item_name)
     end
   end
+
+  def total_inventory
+    total_inventory = {}
+
+    vendors.each do |vendor|
+      vendor.inventory.each do |item, quantity|
+        if total_inventory[item].nil?
+          total_inventory[item] = {quantity: 0, vendors: []}
+        end
+        total_inventory[item][:quantity] += quantity
+        total_inventory[item][:vendors] << vendor
+      end
+    end
+    total_inventory
+  end
+
+  def sorted_item_list
+    items = vendors.map do |vendor|
+      vendor.inventory.keys
+    end.flatten.uniq
+    items.map do |item|
+      item.name
+    end.sort
+  end
+
+  def overstocked_items
+    overstocked = total_inventory.flat_map do |item, details|
+      if details[:quantity] > 50 &&
+      details[:vendors].count > 1
+          item
+      end
+    end
+    overstocked.compact
+  end
 end
